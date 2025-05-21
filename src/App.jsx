df1dd8451f3d734b5854a0cfb43953f5ba0422aa
Andres Soria <andres.zov@gmail.com>
2025-05-21 14:43:57 -0600

    Add useState to generate new numbers and map over them on Die element

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './index.css';
 import Die from './components/Die';
 
 function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
+
   function generateAllNewDice() {
     return new Array(10).fill().map(() => Math.ceil(Math.random() * 6));
   }
 
-  console.log(generateAllNewDice());
+  const diceElements = dice.map((num) => <Die value={num} />);
 
   return (
     <main className='app'>
@@ -16,18 +18,7 @@ function App() {
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
-      <div className='dice-container'>
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={7} />
-        <Die value={8} />
-        <Die value={9} />
-        <Die value={10} />
-      </div>
+      <div className='dice-container'>{diceElements}</div>
     </main>
   );
 }
