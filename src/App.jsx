364ae02a2d815be2a868e73a7b037b7f700dad9b
Andres Soria <andres.zov@gmail.com>
2025-05-21 14:38:41 -0600

    Import dummy dice lement to test generateAllNewDice function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-import './index.css'
+import { useState } from 'react';
+import './index.css';
+import Die from './components/Die';
 
 function App() {
+  function generateAllNewDice() {
+    return new Array(10).fill().map(() => Math.ceil(Math.random() * 6));
+  }
+
+  console.log(generateAllNewDice());
+
   return (
-    <div className="app">
+    <main className='app'>
       <h1>Tenzies</h1>
-      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-    </div>
-  )
+      <p>
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className='dice-container'>
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={7} />
+        <Die value={8} />
+        <Die value={9} />
+        <Die value={10} />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
