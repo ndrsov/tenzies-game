d9c99a6db80686ead35cec63041be29b0ea77ecd
Andres Soria <andres.zov@gmail.com>
2025-05-22 13:38:25 -0600

    Change rollDice function to generate a new game upon completion

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,15 @@ function App() {
     }));
   }
   function rollDice() {
-    setDice((oldDice) =>
-      oldDice.map((die) =>
-        die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
-      )
-    );
+    if (!gameWon) {
+      setDice((oldDice) =>
+        oldDice.map((die) =>
+          die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    } else {
+      setDice(generateAllNewDice());
+    }
   }
 
   function hold(id) {
